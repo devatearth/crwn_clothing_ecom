126554380d43e6a513b23cd53fb989efc1f0da4b
devatearth <64774809+devatearth@users.noreply.github.com>
2022-10-28 12:23:49 +0530

    Moved usecontext to usereducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { createContext, useState } from "react";
+import { useEffect, createContext, useReducer } from "react";
 import { onUserAuthChangedLister, createUserDocumentFromAuth } from "../utils/firebase/firebase-utils";
 
 export const UserContext = createContext({
@@ -7,9 +6,27 @@ export const UserContext = createContext({
     currentUser: null,
 });
 
+const INTIAL_STATE = {
+    currentUser: null,
+}
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case 'SET_CURRENT_USER':
+            return { ...state, currentUser: payload };
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+}
+
 
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [state, dispatch] = useReducer(userReducer, INTIAL_STATE);
+    const { currentUser } = state;
+    const setCurrentUser = (user) =>
+        dispatch({ type: 'SET_CURRENT_USER', payload: user });
     const value = { currentUser, setCurrentUser };
 
     useEffect(() => {
@@ -18,7 +35,7 @@ export const UserProvider = ({ children }) => {
             setCurrentUser(user);
         });
         return unSubscribe;
-    },[])
+    }, [])
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
