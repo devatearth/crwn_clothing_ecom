a37c8e2eb1e334b627d099f8caa9220297432076
devatearth <64774809+devatearth@users.noreply.github.com>
2022-11-06 21:26:19 +0530

    Added redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,19 @@
-import { compose , createStore , applyMiddleware } from "redux";
+import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
 const middleWare = [logger];
 const enhancedMiddleWare = compose(applyMiddleware(...middleWare));
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(rootReducer,undefined,enhancedMiddleWare);
+export const store = createStore(persistedReducer, undefined, enhancedMiddleWare);
+
+export const persistor = persistStore(store);
